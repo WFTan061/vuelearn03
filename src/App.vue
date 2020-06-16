0375d142b6c82e8b854dec70e36b9ef6cfe3edd5
Wua Fong Tan <wuafong92@live.com.sg>
2020-06-16 12:26:15 +0800

    Chart should be completed, missing reporting of above/less than average

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@
     <router-link to = "/UserSummary">Summary</router-link> |
     <router-link to = "/chart">Chart</router-link>
   </nav>
-  <router-view v-bind:selected = "selected" v-bind:theData = "theData" v-bind:average = "averageSpending"></router-view>
+  <router-view v-bind:selected = "selected" v-bind:theData = "theData" v-bind:average = "averageSpending" v-bind:chartData = "chartData" @updateChart="updateChartData"></router-view>
   </div>
 </template>
 <style>
@@ -44,14 +44,44 @@ import axios from 'axios';
         selectOptions:{},
         averageSpending:0,
         selected:null,
+        chartData: { // for 'data' prop of 'bar-chart'
+          categories: ['January'],
+          series: {
+            line:[
+            {
+              name:'Average',
+              data:[0,0],
+            }
+            ],
+            column:[
+            {
+              name:'Spending',
+              data:[0]    
+            },
+            ]
+          }
+        }
+      }
+    },
+    methods:{
+      updateChartData:function(){
+        if(this.selected!==null){
+          this.chartData.series.column[0].data = [this.selectOptions[this.selected].balance];
+          this.chartData.series.column[0].name = this.selectOptions[this.selected].text;
+        }else{
+          this.chartData.series.column[0].name = "None selected"
+          this.chartData.series.column[0].data = [0];
+        }
       }
     },
     mounted:function(){
       const baseURI = 'https://randomapi.com/api/6de6abfedb24f889e0b5f675edc50deb?fmt=raw&sole';
+      //Use axios to access the api.
       axios.get(baseURI)
       .then((result) => {
         this.theData = result.data;
         let totalSpending = 0;
+        //populate form select options.
         this.selectOptions = this.theData.map(function(data,index){
           let fullName = data.first + " " + data.last;
           let balance = data.balance;
@@ -61,15 +91,19 @@ import axios from 'axios';
           //parse data and add to total spending
           totalSpending += parseFloat(balance);
 
-          //map data to select options
+          //create select options
           return{
             text:fullName,
             value:index,
+            balance: balance,
           }
         })
         //Calculate average spending
         this.averageSpending = (totalSpending/this.theData.length).toFixed(2);
+        //update chart data.
+        this.chartData.series.line[0].data = [this.averageSpending,this.averageSpending];
       })      
-    }
+    },
+
   }
 </script>
--- src/views/UserChart.vue
+++ src/views/UserChart.vue
@@ -1,47 +1,26 @@
 <template>
 	<div>
-		<combo-chart v-if = "selected!==null" :data="chartData"/>
+		<combo-chart ref = "chart1" :data="chartData"/>
+		<!-- if chartdata > average, output higher -->
+		<!-- else output lower. -->
 	</div>
 </template>
 
 <script>
 import 'tui-chart/dist/tui-chart.css'
 import { comboChart } from '@toast-ui/vue-chart'
-
 export default {
     components: {
         'combo-chart': comboChart
     },
-    props:['selected','theData','average'],
-    data: function() {
-		return {
-			chartData: { // for 'data' prop of 'bar-chart'
-				categories: ['January'],
-				series: {
-					line:[
-					{
-						name:'Average',
-						data:[this.average,this.average],
-					}
-					],
-					column:[
-					{
-						name:'Spending',
-						data:[1]		
-					},
-					]
-				}
-			}
-		}
-	},
+    props:['selected','theData','average','chartData'],
 	watch:{
-		theData: function(){
-			console.log(this.average);
-			//perform function to update combochart.
-		},
+		//apply changes when 
 		selected: function(){
-			console.log(this.selected)
-			//perform function to update combochart.
+			//set new data to prevent vue from thinking nothing changed 
+			console.log(Object.keys(this.chartData))
+			console.log(this.chartData)
+			this.$emit('updateChart');
 		},
 	}
 }
